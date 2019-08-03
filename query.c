b842413730f01a233b6242e5f649079be9c0cc4c
swarnav999 <swarnav999@gmail.com>
2019-08-03 10:29:31 +1000

    Update query.c

--- query.c
+++ query.c
@@ -27,13 +27,119 @@ Query startQuery(Reln r, char *q)
 {
 	Query new = malloc(sizeof(struct QueryRep));
 	assert(new != NULL);
-	// TODO
+	
+		// TODO
 	// Partial algorithm:
 	// form known bits from known attributes
 	// form unknown bits from '?' attributes
 	// compute PageID of first page
 	//   using known bits and first "unknown" value
 	// set all values in QueryRep object
+	
+	//added by Swarnava
+	Count nvals = nattrs(r);
+	int nstars = 0;
+	char *q0 = q;
+	int a,b;
+	//Tuple t = q;
+	//Bits tuple_hash = tupleHash(r,t);
+	Bits composite_hash = 0,not_known = 0, oneBit;
+	ChVecItem *cv = chvec(r);
+	
+	char **attris = malloc(nvals * sizeof(char *));
+	int i = 0;
+	for(;;){
+	  while(*q != ',' && *q != '\0') q++;
+	  if(*q == '\0'){
+	    //end of query string; add last field to attris
+	    attris[i++] = copyString(q0);
+	    break;
+	  }
+	  else{
+	    //end of next field; add to attris
+	    *q = '\0';
+	    attris[i++] = copyString(q0);
+	    *q = ',';
+	    q++; q0 = q;
+	  }
+	}
+	
+	Bits h[nvals + 1];
+	for(i = 0;i < nvals; i++){
+	  h[i] = hash_any((unsigned char *)attris[i],strlen(attris[i]));
+	  char buff[MAXBITS + 1];
+	  bitsString(h[i],buff);
+	   printf("%s\n",buff);
+	   //printf("%s",attris[i]);
+	}
+	
+	
+	for(i = 0;i < nvals; i++){
+	  if(*attris[i] != '?'){
+	    //set d[i] bits in composite hash
+	    for(int j =0;j < MAXBITS; j++){
+	      a = cv[j].att;
+	      b = cv[j].bit;
+	      if(a == i){
+		oneBit = bitIsSet(h[a],b);
+		//printf("%d value \n",j);
+		composite_hash = composite_hash | (oneBit<<j);
+	      //bit_count++;
+	      }
+	    }
+	    //bit_count = bit_count + depth(r);
+	  }
+	  
+	  else{
+	    //for(int k = bit_count;k < bit_count + depth(r);k++){
+	    for(int k = 0; k < MAXBITS; k++){  
+	      a = cv[k].att;
+	      b = cv[k].bit;
+	      if(a == i){
+		not_known = not_known |(1 << k);
+		nstars++;
+	      }
+	      //else{
+	      //oneBit = bitIsSet(h[a],b);
+	      //composite_hash = composite_hash |  (oneBit << k);
+		
+	      //}
+	    }
+	    //bit_count = bit_count + depth(r);
+	    
+	  }
+	  
+	  
+	  // printf("%s",attris[i]);
+	}
+	char buffer[MAXBITS];
+	bitsString(composite_hash,buffer);
+	//printf("Bit COUUUNT %d\n",bit_count);
+	printf("KNOWN %s\n",buffer);
+	
+	char uknown[MAXBITS];
+	bitsString(not_known,uknown);
+	printf("UKNOWN %s\n",uknown);
+	
+	printChVec(chvec(r));
+	
+	new->known = composite_hash;
+	new->unknown = not_known;
+	new->rel = r;
+	new->curpage = 0;
+	//find first page
+	//char unknown[MAXBITS];
+	//int value_of_bit;
+	//bitsString(new->unknown, unknown);
+	//for(i = 0; i < depth(r), i++){
+	// 
+	//}
+	
+	new->is_ovflow = 0;
+	new->curtup = 0;
+	
+	
+
 	return new;
 }
 
